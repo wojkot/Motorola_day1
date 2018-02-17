3938f13b1c51fa903ffce6174ba4be7b09be2242
wkot <wojciech.kot93@gmail.com>
2018-02-17 09:52:55 +0100

    Added test class with unit test

---
+++ Solution/Solution/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    public class TestClass
+    {
+        public static string GetMessage()
+        {
+            return "Test message";
+        }
+    }
+}
---
+++ Solution/SolutionTests/TestClassTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.Tests
+{
+    [TestClass()]
+    public class TestClassTests
+    {
+        [TestMethod()]
+        public void GetMessageTest()
+        {
+            string givenString = "Test message";
+
+            Assert.AreEqual(givenString, TestClass.GetMessage());
+        }
+    }
+}
